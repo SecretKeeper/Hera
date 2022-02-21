fc62addd1df3b1392e3e4e046802372679478c99
Secret Keeper <shervin.ourfamily@gmail.com>
2022-02-21 03:41:15 +0330

    init

---
+++ src/repositories/db.rs
@@ -0,0 +1,15 @@
+extern crate diesel;
+
+use actix::Actor;
+use actix::SyncContext;
+
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    PgConnection,
+};
+
+pub struct DbExecutor(pub Pool<ConnectionManager<PgConnection>>);
+
+impl Actor for DbExecutor {
+    type Context = SyncContext<Self>;
+}
