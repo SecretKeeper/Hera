832dabf56a129b54d233d75f8318c09ccd843cb2
Secret Keeper <shervin.ourfamily@gmail.com>
2022-02-21 15:05:02 +0330

    hash password b4 save

--- src/repositories/user_repository.rs
+++ src/repositories/user_repository.rs
@@ -1,6 +1,7 @@
 use crate::models::{CreateUser, User};
 use crate::schema::users::dsl::*;
 use actix::{Handler, Message, SyncContext};
+use blake3::Hasher;
 use diesel::associations::HasTable;
 use diesel::prelude::*;
 use diesel::result::Error;
@@ -14,8 +15,14 @@ impl Message for CreateUser {
 impl Handler<CreateUser> for DbExecutor {
     type Result = Result<User, Error>;
 
-    fn handle(&mut self, new_user: CreateUser, _: &mut SyncContext<Self>) -> Self::Result {
+    fn handle(&mut self, mut new_user: CreateUser, _: &mut SyncContext<Self>) -> Self::Result {
         let conn: &PgConnection = &self.0.get().unwrap();
+
+        let mut hasher = Hasher::new();
+
+        hasher.update(&new_user.password.as_bytes());
+
+        new_user.password = hasher.finalize().to_hex().chars().collect();
 
         let inserted_user = diesel::insert_into(users::table())
             .values(&new_user)
