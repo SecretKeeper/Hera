6dab52e118eb73d5964be703e439355b0d6406a9
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-01 16:45:26 +0330

    removed duplicated struct

--- src/models/email.rs
+++ src/models/email.rs
@@ -1,8 +1,1 @@
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ChangeEmailRequest {
-    pub uid: Option<i32>,
-    pub current_password: String,
-    pub new_password: String,
-}
