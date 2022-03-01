4d1fdac16b1fbda7eabca74d8246d4cb857da8ca
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-01 16:28:50 +0330

    implement messagehandler for ChangeUsernameRequest

--- src/message_handlers/settings_handler.rs
+++ src/message_handlers/settings_handler.rs
@@ -1,9 +1,9 @@
-use crate::db::DbExecutor;
 use crate::diesel::ExpressionMethods;
 use crate::errors::ServiceError;
 use crate::models::settings::ChangePasswordRequest;
 use crate::schema::users::dsl::*;
 use crate::utils::hash::hash_string;
+use crate::{db::DbExecutor, models::settings::ChangeUsernameRequest};
 
 use actix::{Handler, Message, SyncContext};
 use diesel::{PgConnection, QueryDsl, RunQueryDsl};
@@ -36,5 +36,36 @@ impl Handler<ChangePasswordRequest> for DbExecutor {
         }
 
         Ok("Password updated successfully".to_string())
+    }
+}
+
+impl Message for ChangeUsernameRequest {
+    type Result = Result<String, ServiceError>;
+}
+
+impl Handler<ChangeUsernameRequest> for DbExecutor {
+    type Result = Result<String, ServiceError>;
+
+    fn handle(
+        &mut self,
+        change_username_request: ChangeUsernameRequest,
+        _: &mut SyncContext<Self>,
+    ) -> Self::Result {
+        let conn: &PgConnection = &self.0.get().unwrap();
+
+        let target = users
+            .filter(id.eq(change_username_request.uid.unwrap()))
+            .filter(password.eq(hash_string(change_username_request.current_password)));
+
+        let updated_row = diesel::update(target)
+            .set(username.eq(change_username_request.new_username))
+            .execute(conn)
+            .expect("cant change username");
+
+        if updated_row == 0 {
+            return Err(ServiceError::Forbidden);
+        }
+
+        Ok("Username updated successfully".to_string())
     }
 }
