a5de4028369b39c78610fdc088232c25ef76ad0c
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-01 16:45:48 +0330

    add ChangeEmailRequest struct

--- src/models/settings.rs
+++ src/models/settings.rs
@@ -10,6 +10,13 @@ pub struct ChangePasswordRequest {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ChangeUsernameRequest {
     pub uid: Option<i32>,
-    pub current_password: String,
+    pub password: String,
     pub new_username: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChangeEmailRequest {
+    pub uid: Option<i32>,
+    pub password: String,
+    pub new_email: String,
 }
