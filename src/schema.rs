1d22f9c486ec004e14ececee352d049774437609
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-02 18:14:31 +0330

    add profiles schema

--- src/schema.rs
+++ src/schema.rs
@@ -10,6 +10,18 @@ table! {
 }
 
 table! {
+    profiles (id) {
+        id -> Int4,
+        user_id -> Int4,
+        status -> Nullable<Varchar>,
+        description -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Nullable<Timestamp>,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         solana_pubkey -> Nullable<Varchar>,
@@ -24,4 +36,8 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(jwt_tokens, users,);
+allow_tables_to_appear_in_same_query!(
+    jwt_tokens,
+    profiles,
+    users,
+);
