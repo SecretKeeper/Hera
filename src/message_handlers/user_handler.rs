dd9cde37cabe1c8cbcd083a5876a1176233d6dc4
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 15:44:13 +0330

    create whisper profile after registering user

--- src/message_handlers/user_handler.rs
+++ src/message_handlers/user_handler.rs
@@ -1,5 +1,6 @@
 use crate::db::DbExecutor;
-use crate::models::user::{CreateUser, User};
+use crate::models::user::{CreateProfile, CreateUser, User};
+use crate::schema::profiles::dsl::*;
 use crate::schema::users::dsl::*;
 use actix::{Handler, Message, SyncContext};
 use blake3::Hasher;
@@ -17,16 +18,27 @@ impl Handler<CreateUser> for DbExecutor {
     fn handle(&mut self, mut new_user: CreateUser, _: &mut SyncContext<Self>) -> Self::Result {
         let conn: &PgConnection = &self.0.get().unwrap();
 
+        let whisper_conn: &PgConnection = &self.1.get().unwrap();
+
         let mut hasher = Hasher::new();
 
         hasher.update(&new_user.password.as_bytes());
 
         new_user.password = hasher.finalize().to_hex().chars().collect();
 
-        let inserted_user = diesel::insert_into(users::table())
+        let inserted_user: User = diesel::insert_into(users::table())
             .values(&new_user)
             .get_result(conn)
             .expect("cant insert user");
+
+        // Create also user profile on Whisper App
+        let _create_whisper_profile = diesel::insert_into(profiles::table())
+            .values(CreateProfile {
+                user_id: inserted_user.id,
+                status: "".to_string(),
+                description: "".to_string(),
+            })
+            .execute(whisper_conn);
 
         Ok(inserted_user)
     }
