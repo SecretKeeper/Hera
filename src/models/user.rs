0e6cbe1e7a63d7b819ca56e7edd90454a61d5e98
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 15:43:13 +0330

    add CreateProfile struct & make some fields hidden

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,19 +1,23 @@
+use crate::schema::profiles;
 use crate::schema::users;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Debug, Queryable, Identifiable, Serialize, Deserialize)]
+#[derive(AsChangeset, Debug, Queryable, Identifiable, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub solana_pubkey: Option<String>,
     pub ethereum_pubkey: Option<String>,
     pub username: Option<String>,
     pub email: Option<String>,
+    #[serde(skip_serializing)]
     pub password: Option<String>,
     pub avatar: Option<String>,
     pub created_at: NaiveDateTime,
+    #[serde(skip_serializing)]
     pub updated_at: Option<NaiveDateTime>,
+    #[serde(skip_serializing)]
     pub deleted_at: Option<NaiveDateTime>,
 }
 
@@ -24,6 +28,14 @@ pub struct CreateUser {
     pub email: String,
     #[validate(length(min = 8))]
     pub password: String,
+}
+
+#[derive(Debug, Validate, Insertable, Serialize, Deserialize)]
+#[table_name = "profiles"]
+pub struct CreateProfile {
+    pub user_id: i32,
+    pub status: String,
+    pub description: String,
 }
 
 #[derive(Debug, Serialize)]
